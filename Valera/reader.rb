d077cbae25906b908eb8e5c3041c0cedcc1787b8
oxeq <xotaby4@yandex.ru>
2021-12-06 18:35:04 +0300

    Add input tests

--- Valera/reader.rb
+++ Valera/reader.rb
@@ -1,10 +1,11 @@
 class Reader
   def read_action(game)
     loop do
-      game.action_item = gets.chomp.to_i
-      return unless (game.action_item < 1) || (game.action_item > 10)
+      game.action_item = $stdin.gets.chomp.to_i
+      return true unless (game.action_item < 1) || (game.action_item > 10)
 
       puts('Ошибка! Такого действия нет. Введите значение в диапозоне от 1 до 10')
+      return false
     end
   end
 end
---
+++ Valera/spec/reader_spec.rb
@@ -0,0 +1,41 @@
+require './reader'
+
+describe '.reader' do
+
+  context 'read_action_true_1' do
+    it {
+      game = Game.new
+      reader = Reader.new
+      allow($stdin).to receive(:gets).and_return('1')
+      expect(reader.read_action(game)).to eq true
+    }
+  end
+
+  context 'read_action_false_1' do
+    it {
+      game = Game.new
+      reader = Reader.new
+      allow($stdin).to receive(:gets).and_return('*')
+      expect(reader.read_action(game)).to eq false
+    }
+  end
+
+  context 'read_action_true_2' do
+    it {
+      game = Game.new
+      reader = Reader.new
+      allow($stdin).to receive(:gets).and_return('10')
+      expect(reader.read_action(game)).to eq true
+    }
+  end
+
+  context 'read_action_false_2' do
+    it {
+      game = Game.new
+      reader = Reader.new
+      allow($stdin).to receive(:gets).and_return('0')
+      expect(reader.read_action(game)).to eq false
+    }
+  end
+
+end
