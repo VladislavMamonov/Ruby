416be663e5b29272b22f76673c31134a91fda16c
GenaBukin3000 <lbart008@gmail.com>
2021-12-06 20:02:37 +0700

    Add Valera tests

---
+++ Valera/spec/valera_spec.rb
@@ -0,0 +1,25 @@
+require './valera'
+
+describe '.valera_check' do
+  valera = Valera.new
+
+  context 'check_true' do
+    status = { 'mana' => 30, 'happiens' => 5, 'stamina' => 0, 'money' => 10_000 }
+    it { expect(valera.check_status(status)).to eq true }
+  end
+
+  context 'check_false' do
+    status = { 'mana' => 250, 'happiens' => 5, 'stamina' => 0, 'money' => 10_000 }
+    it { expect(valera.check_status(status)).to eq false }
+  end
+
+  context 'check_false' do
+    status = { 'mana' => 250, 'happiens' => 5, 'stamina' => 0, 'money' => 10_000 }
+    it { expect(valera.check_status(status)).to eq false }
+  end
+
+  context 'check_false' do
+    status = { 'mana' => 30, 'happiens' => 5, 'stamina' => 250, 'money' => 10_000 }
+    it { expect(valera.check_status(status)).to eq false }
+  end
+end
