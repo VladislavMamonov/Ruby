d39610eb6723fd7b89b94bcb78d783265ec64267
Nichidanil <nichi.par@mail.ru>
2021-12-06 20:10:19 +0700

    Add save test

---
+++ Valera/spec/saver_spec.rb
@@ -0,0 +1,16 @@
+require './saver'
+
+RSpec.describe Saver do
+  describe '.saver' do
+    context 'save_load_menu' do
+      status = { 'mana' => 10, 'happiens' => -10, 'stamina' => 25, 'money' => 2500 }
+
+      it {
+        allow($stdin).to receive(:gets).and_return('test_save')
+        Saver.save_load_menu(status, 7)
+        curr_status = Saver.save_load_menu(status, 8)
+        expect(curr_status).to eq status
+      }
+    end
+  end
+end
